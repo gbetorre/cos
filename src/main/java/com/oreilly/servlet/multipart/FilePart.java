127be8b84b4d8b3a06f968ce55a202711ea23216
gbetorre <gianroberto.torre@gmail.com>
2025-08-05 13:15:19 +0200

    Changed imports from javax.servlet.* to jakarta.servlet.* (and formatted them)

--- src/main/java/com/oreilly/servlet/multipart/FilePart.java
+++ src/main/java/com/oreilly/servlet/multipart/FilePart.java
@@ -4,13 +4,14 @@
 
 package com.oreilly.servlet.multipart;
 
-import java.io.File;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import javax.servlet.ServletInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import jakarta.servlet.ServletInputStream;
 
 /**
  * A <code>FilePart</code> is an upload part which represents a 
